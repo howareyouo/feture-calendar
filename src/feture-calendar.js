3ae4a324993c35825f3eb4b241bdf0a7ba9df052
backflow <464961588@qq.com>
2018-11-30 19:18:26 +0800

    initial commit

--- src/feture-calendar.js
+++ src/feture-calendar.js
@@ -11,12 +11,14 @@
   'use strict'
 
   var defaults = {
+    months: 3,
+    range: ['入住', '离店'],
     i18n: {
-      monthes: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
-      weeks: ['日', '一', '二', '三', '四', '五', '六']
+      months: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+      weeks: ['日', '一', '二', '三', '四', '五', '六'],
+      today: '今天'
     },
     onSelect: function (date) {
-      console.log(date)
     }
   }
 
@@ -34,108 +36,148 @@
     return el
   }
 
+  function createDayElement (date, disabled, today, todayText) {
+    var dayEl = document.createElement('em')
+    var numEl = document.createElement('b')
+    numEl.innerHTML = date.getDate()
+    dayEl.className = 'fc-day'
+    dayEl.time = date.getTime()
+
+    if (disabled) {
+      dayEl.classList.add('fc-day-disabled')
+    }
+
+    if (today) {
+      dayEl.classList.add('fc-day-today')
+      numEl.innerHTML = todayText
+    }
+
+    dayEl.appendChild(numEl)
+    return dayEl
+  }
+
+  function appendDays (when, now, options, container) {
+    var disabled = options.disablePast && when.getTime() <= now.getTime() - 1,
+        today    = when.toDateString() === now.toDateString(),
+        weekday  = when.getDay(),
+        day      = when.getDate()
+
+    var dayEl = createDayElement(when, disabled, today, options.i18n.today)
+
+    // if it's the first day of the month
+    if (day == 1 && weekday != 0) {
+      dayEl.style.marginLeft = weekday * 14.2857 + '%'
+      // for (var i = 0; i < weekday; i++)
+      // this.month.appendChild(this.createDayElement(''))
+    }
+    container.appendChild(dayEl)
+    return container
+  }
+
+  function removeActiveClass (el) {
+    el.querySelectorAll('.fc-day-selected').forEach(function (e) {
+      e.classList.remove('fc-day-selected')
+      e.classList.remove('fc-range-from')
+      e.classList.remove('fc-range-to')
+    })
+  }
+
+  function rangeSelect (tar, range, elems, ctx) {
+    if (elems.length < 2) {
+      tar.classList.add('fc-day-selected')
+      elems.push(tar)
+      if (elems.length == 2) {
+        elems.sort(function (a, b) {
+          return a.time - b.time
+        })
+        elems[0].classList.add('fc-range-from')
+        elems[1].classList.add('fc-range-to')
+      }
+    } else {
+      singleSelect(tar, elems, ctx)
+    }
+    console.log(elems)
+  }
+
+  function singleSelect (tar, elems, ctx) {
+    removeActiveClass(ctx)
+    tar.classList.add('fc-day-selected')
+    elems.length = 0
+    elems.push(tar)
+  }
+
   function Plugin (el, options) {
     this.options = Object.assign(defaults, options)
-    this.init(el)
+    this.elems = []
+    this.date = new Date()
+    this.date.setDate(1)
+    this.el = el
+    this.init()
   }
 
   var clickHandler = function (e) {
-    var target = e.target,
-        tag    = target.tagName
+    var tar = e.target,
+        tag = tar.tagName
     if (tag != 'EM' && tag != 'B') {
       return
     }
     if (tag == 'B') {
-      target = target.parentNode
+      tar = tar.parentNode
     }
-    if (target.classList.contains('fc-day-disabled')) {
+    if (tar.classList.contains('fc-day-disabled')) {
       return
     }
-    this.options.onSelect(new Date(target.date))
-    this.removeActiveClass()
-    target.classList.toggle('fc-day-selected')
+
+    var date = new Date(tar.time)
+    this.options.onSelect(date)
+
+    var range = this.options.range,
+        elems = this.elems
+
+    if (range && range.length == 2) {
+      rangeSelect(tar, range, elems, this.el)
+    } else {
+      singleSelect(tar, elems, this.el)
+    }
   }
 
   Plugin.prototype = {
 
-    init: function (el) {
-      this.today = new Date()
-      this.date = new Date()
-      this.date.setDate(1)
-      el.classList.add('fc')
+    init: function () {
+      this.el.classList.add('fc')
       var weeks = this.options.i18n.weeks
       var week = createElement('div', 'fc-week')
       for (var i in weeks) {
         week.appendChild(createElement('span', null, weeks[i]))
       }
       this.body = createElement('div', 'fc-body')
-      el.appendChild(week)
-      el.appendChild(this.body)
-      this.appendMonths(3)
+      this.el.appendChild(week)
+      this.el.appendChild(this.body)
+      this.appendMonths(this.options.months)
       this.attachListeners()
     },
 
     appendMonths: function (n) {
-      var date = this.date
+      var date = this.date,
+          now  = new Date()
       while (n--) {
         var current = date.getMonth()
-        var month = createElement('div', 'fc-month', date.getFullYear() + '年' + (date.getMonth() + 1) + '月')
-        var days = createElement('div', 'fc-days')
-        this.body.appendChild(month)
+        var monthEl = createElement('div', 'fc-month', date.getFullYear() + '年' + (date.getMonth() + 1) + '月')
+        var daysEl = createElement('div', 'fc-days')
+        this.body.appendChild(monthEl)
         while (date.getMonth() === current) {
-          this.appendDays(date.getDate(), date.getDay(), days)
+          appendDays(date, now, this.options, daysEl)
           date.setDate(date.getDate() + 1)
         }
-        this.body.appendChild(days)
+        this.body.appendChild(daysEl)
       }
       // while loop trips over and day is at 30/31, bring it back
       date.setDate(1)
       date.setMonth(date.getMonth() - 1)
     },
 
-    appendDays: function (day, weekday, container) {
-      var disabled = this.options.disablePast && this.date.getTime() <= this.today.getTime() - 1
-      var today = this.date.toDateString() === this.today.toDateString()
-      var newDay = this.createDayElement(day, disabled, today)
-
-      // if it's the first day of the month
-      if (day == 1 && weekday != 0) {
-        newDay.style.marginLeft = weekday * 14.2857 + '%'
-        // for (var i = 0; i < weekday; i++)
-        // this.month.appendChild(this.createDayElement(''))
-      }
-      container.appendChild(newDay)
-      return container
-    },
-
-    createDayElement: function (day, disabled, today) {
-      var dayEl = document.createElement('em')
-      var numEl = document.createElement('b')
-      numEl.innerHTML = day
-      dayEl.className = 'fc-day'
-      dayEl.date = this.date.getTime()
-
-      if (disabled) {
-        dayEl.classList.add('fc-day-disabled')
-      }
-
-      if (today) {
-        dayEl.classList.add('fc-day-today')
-        numEl.innerHTML = '今天'
-      }
-
-      dayEl.appendChild(numEl)
-      return dayEl
-    },
-
     attachListeners: function () {
       this.body.addEventListener('click', clickHandler.bind(this))
-    },
-
-    removeActiveClass: function () {
-      document.querySelectorAll('.fc-day-selected').forEach(function (e) {
-        e.classList.remove('fc-day-selected')
-      })
     },
 
     destroy: function () {
